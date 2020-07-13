12d82b652c422af03a579eb65c73488f5a259566
1051062633@qq.com <a272866589>
2020-07-13 21:22:35 +0800

    0713 commit

---
+++ src/main/java/com/example/ysadmin/YsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.ysadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YsAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ysadmin/YsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ysadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
